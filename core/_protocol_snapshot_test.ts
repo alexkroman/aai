7c77ddc79256b14b4cd9df65b2f9048796823506
Alex Kroman <alex@assemblyai.com>
2026-03-11 10:03:14 -0700

    Remove unneeded snapshots tests

--- core/_protocol_snapshot_test.ts
+++
@@ -1,6 +0,0 @@
-import { assertSnapshot } from "@std/testing/snapshot";
-import { AudioFrameSpec } from "./_protocol.ts";
-
-Deno.test("AudioFrameSpec snapshot", async (t) => {
-  await assertSnapshot(t, AudioFrameSpec);
-});
